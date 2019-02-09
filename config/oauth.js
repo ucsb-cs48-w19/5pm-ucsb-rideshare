c17fd71d7b47523c3b6a3ee9e7926d932f66b257
SihuaMa12 <sihua@umail.ucsb.edu>
2019-02-08 17:36:47 -0800

    changed the name of consumerID to clientID

--- config/oauth.js
+++ config/oauth.js
@@ -6,8 +6,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //   credentials (in this case, a token, tokenSecret, and Google profile), and
 //   invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    consumerKey: "991786945525-bbgk5uf02crusqoqvfafvojq8n1qg1sq.apps.googleusercontent.com",
-    consumerSecret: "_H7REb5_6JFeq0nZRIm-nPY7",
+    cliendID: "991786945525-bbgk5uf02crusqoqvfafvojq8n1qg1sq.apps.googleusercontent.com",
+    clientSecret: "_H7REb5_6JFeq0nZRIm-nPY7",
     callbackURL: "https://ucsb-rideshare-w19.herokuapp.com/callback"
   },
   function(token, tokenSecret, profile, done) {
