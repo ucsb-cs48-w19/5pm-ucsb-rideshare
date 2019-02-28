354de1d1005008efe14b0b313798788568f2d150
SihuaMa12 <sihua@umail.ucsb.edu>
2019-02-27 19:40:18 -0800

    MS added functionality for filtering(basic)

--- routes/rides.js
+++ routes/rides.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const models = require("../models/index.js");
 const authCheck = require("../authCheck.js");
 const Sequelize = require("sequelize");
+const sortingFunctions = require("../sorting.js");
 
 // Since we're using router the / refers to /rides
 router.get("/", function(request, response) {
@@ -114,8 +115,10 @@ router.get("/my_rides", authCheck, function(request, response) {
 
 router.get("/search", function(request, response) {
 
-	let {term} = request.query
-
+	let {term, filter} = request.query;
+	
+	if(!filter)
+	{
 	models.Ride.findAll( {where: {destination: { [Sequelize.Op.like]: "%" + term + "%" }}})
 	.then(function(rides) {
 		response.render("rides", {
@@ -126,6 +129,28 @@ router.get("/search", function(request, response) {
 	.catch(function(err) {
 		console.log(err);
 	})
+	}
+	else
+	{
+		let {filterdate, filterprice,filtertime}=request.query;
+		var condition={};
+		condition.destination={ [Sequelize.Op.like]: "%" + term + "%" };
+		if(filterdate)
+			condition.date={[Sequelize.Op.lte]: filterdate}
+		if(filterprice)
+			condition.price={[Sequelize.Op.lte]: filterprice}
+		if(filtertime)
+			condition.time={[Sequelize.Op.lte]: filtertime}
+		models.Ride.findAll({where: condition, raw: true})
+		.then(rides=>{
+			rides.sort(sortingFunctions.sortByDateTimePrice);
+			
+			response.render("rides",{
+				user:request.user,
+				rides:rides,
+			});
+		});
+	}
 });
 
 
